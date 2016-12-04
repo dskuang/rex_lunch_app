b289270340e66a2dc59e0d8aaa9f6042ac91e368
Dominic Kuang <dskuang@gmail.com>
2016-12-03 19:29:19 -0800

    fixing user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,16 +1,11 @@
 class UserMailer < ApplicationMailer
   attr_reader :restaurant, :time
-  SF_EMAIL = Email.of_group('global').addresses
-
-  INDIVIDUAL_EMAILS = Email.of_group('individual').addresses
-
-  TEST_EMAILS = Email.of_group('test').addresses
 
   def send_lunch_email(restaurant, time)
     @time = time
     @restaurant = restaurant
     mail(
-      to: TEST_EMAILS,
+      to: individual_emails,
       subject: subject,
       template_name: 'send_lunch_email'
     )
@@ -20,5 +15,17 @@ class UserMailer < ApplicationMailer
 
   def subject
     "#{restaurant.name.titleize} - #{time || Date.upcoming_lunch_day.strftime('%A, %B %e')}"
+  end
+
+  def sf_email
+    Email.of_group('global').addresses
+  end
+
+  def individual_emails
+    Email.of_group('individual').addresses
+  end
+
+  def test_emails
+    Email.of_group('test').addresses
   end
 end
